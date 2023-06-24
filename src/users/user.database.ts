8448b935c8bebafea5534c15d6083b8764ed388c
kritsanapr <kritsana.pr.41@gmail.com>
2023-06-25 00:13:00 +0700

    feat : develop feature user database.

--- src/users/user.database.ts
+++ src/users/user.database.ts
@@ -1,6 +1,6 @@
 import { User, UnitUser, Users } from './user.interface';
 import bcrypt from 'bcryptjs';
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as random } from 'uuid';
 import fs from 'fs';
 
 let users: Users = loadUsers();
@@ -14,3 +14,111 @@ function loadUsers() {
         return {};
     }
 }
+
+
+function saveUsers() {
+    try {
+        fs.writeFileSync("./users.json", JSON.stringify(users), "utf-8")
+        console.log("Users saved");
+    } catch (err) {
+        console.log(`Error ${err}`);
+    }
+}
+
+export const findAll = async (): Promise<UnitUser[]> => Object.values(users);
+
+export const findOne = async (id: string): Promise<UnitUser> => users[id];
+
+export const create = async (userData: UnitUser): Promise<UnitUser | null> => {
+
+    let id = random()
+
+    let check_user = await findOne(id);
+
+    while (check_user) {
+        id = random()
+        check_user = await findOne(id)
+    }
+
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(userData.password, salt);
+
+    const user: UnitUser = {
+        id: id,
+        username: userData.username,
+        email: userData.email,
+        password: hashedPassword
+    };
+
+    users[id] = user;
+
+    saveUsers()
+
+    return user;
+};
+
+export const findByEmail = async (user_email: string): Promise<null | UnitUser> => {
+
+    const allUsers = await findAll();
+
+    const getUser = allUsers.find(result => user_email === result.email);
+
+    if (!getUser) {
+        return null;
+    }
+
+    return getUser;
+};
+
+export const comparePassword = async (email: string, supplied_password: string): Promise<null | UnitUser> => {
+
+    const user = await findByEmail(email)
+
+    const decryptPassword = await bcrypt.compare(supplied_password, user!.password)
+
+    if (!decryptPassword) {
+        return null
+    }
+
+    return user
+}
+
+export const update = async (id: string, updateValues: User): Promise<UnitUser | null> => {
+
+    const userExists = await findOne(id)
+
+    if (!userExists) {
+        return null
+    }
+
+    if (updateValues.password) {
+        const salt = await bcrypt.genSalt(10)
+        const newPass = await bcrypt.hash(updateValues.password, salt)
+
+        updateValues.password = newPass
+    }
+
+    users[id] = {
+        ...userExists,
+        ...updateValues
+    }
+
+    saveUsers()
+
+    return users[id]
+}
+
+export const remove = async (id: string): Promise<null | void> => {
+
+    const user = await findOne(id)
+
+    if (!user) {
+        return null
+    }
+
+    delete users[id]
+
+    saveUsers()
+}
+
--- src/users/user.interface.ts
+++ src/users/user.interface.ts
@@ -1,5 +1,5 @@
 export interface User {
-    user: string;
+    username: string;
     email: string;
     password: string;
 }
